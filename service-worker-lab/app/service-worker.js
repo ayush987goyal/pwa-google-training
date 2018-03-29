5eec2142dd46ea3780e1cff44369998082cddcf3
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-29 14:32:31 +0530

    added service worker lab

--- service-worker-lab/app/service-worker.js
+++ service-worker-lab/app/service-worker.js
@@ -17,9 +17,22 @@ limitations under the License.
   'use strict';
 
   // TODO - 3.1: Add install and activate event listeners
+  self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+
+    // TODO - 3.4: Skip waiting
+    self.skipWaiting();
+  });
+
+  self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+  });
 
   // TODO - 3.3: Add a comment to change the service worker
+  // I'm a new service worker
 
   // TODO - 4: Add fetch listener
-
+  self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+  });
 })();
