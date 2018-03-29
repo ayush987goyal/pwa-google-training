2b43e2dad592aa6e002116d6bbab866f721feef3
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-30 01:10:44 +0530

    added cache api lab section

--- 03-cache-api-lab/app/service-worker.js
+++ 03-cache-api-lab/app/service-worker.js
@@ -19,8 +19,87 @@ limitations under the License.
 
   // TODO 2 - cache the application shell
 
+  var filesToCache = [
+    '.',
+    'style/main.css',
+    'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+    'images/still_life-1600_large_2x.jpg',
+    'images/still_life-800_large_1x.jpg',
+    'images/still_life_small.jpg',
+    'images/still_life_medium.jpg',
+    'index.html',
+    'pages/offline.html',
+    'pages/404.html'
+  ];
+
+  var staticCacheName = 'pages-cache-v2';
+
+  self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntill(
+      caches.open(staticCacheName).then(cache => {
+        cache.addAll(filesToCache);
+      })
+    );
+  });
+
   // TODO 3 - intercept network requests
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => {
+          if (response) {
+            console.log('Found ', event.request.url, ' in cache');
+            return response;
+          }
+
+          console.log('Network request for ', event.request.url);
+          return (
+            fetch(event.request)
+              // TODO 4 - Add fetched files to the cache
+              .then(response => {
+                // TODO 5 - Respond with custom 404 page
+                if (response.status === 404) {
+                  return caches.match('pages/404.html');
+                }
+
+                return caches.open(staticCacheName).then(cache => {
+                  if (event.request.url.indexOf('test') < 0) {
+                    cache.put(event.request.url, response.clone());
+                  }
+                  return response;
+                });
+              })
+          );
+        })
+        .catch(error => {
+          // TODO 6 - Respond with custom offline page
+          return caches.match('pages/offline.html');
+        })
+    );
+  });
 
   // TODO 7 - delete unused caches
 
+  self.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+
+    var cacheWhiteList = [staticCacheName];
+
+    event.waitUntill(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhiteList.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
 })();
